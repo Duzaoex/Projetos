c8507123dbf54c71823a49e50d39957a1d7da367
Duzaoex <eduardoneriscarneiro@gmail.com>
2024-02-28 13:25:31 -0300

    app

--- Projeto App de Receitas/src/App.tsx
+++ Projeto App de Receitas/src/App.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+
+import PageRoutes from './routes';
+
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div className="App">
+      <div className="header-container">
+        <header>
+          <h1>Pokédex</h1>
+          <nav>
+            <Link className="link" to="/">
+              {`Home`}
+            </Link>
+            <Link className="link" to="/about">
+              {`About`}
+            </Link>
+            <Link className="link" to="/favorites">
+              {`Favorite Pokémon`}
+            </Link>
+          </nav>
+        </header>
+      </div>
+      <div className="main-container">
+        <main>
+          <PageRoutes />
+        </main>
+      </div>
     </div>
   );
 }
--- Projeto App de Receitas/vite.config.ts
+++ Projeto App de Receitas/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,12 +16,13 @@ export default defineConfig({
       reporter: ['text', 'json', 'html'],
       include: ['src/**/*'],
       exclude: [
+        "node_modules/*",
         "src/main.tsx",
-        "src/fakes",
-        "src/routes",
-        "src/tests"
+        "src/tests/*",
+        "src/routes/*",
+        "src/renderWithRouter.tsx",
       ],
-      provider: 'c8'
-    }
+      provider: 'c8',
+    },
   },
-})
+});
